15290a98cc008533bc8a951b0b234c55367bb67c
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2019-06-07 14:56:08 +0100

    added get current user helper

---
+++ GossipGastropodsBackEnd/Helpers/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using GossipGastropodsBackEnd.Models;
+
+namespace GossipGastropodsBackEnd.Helpers
+{
+    public static class Extensions
+    {
+        public static CurrentUser GetCurrentUser(this IHttpContextAccessor httpContext)
+        {
+            return new CurrentUser(httpContext);
+        }
+    }
+}
---
+++ GossipGastropodsBackEnd/Models/CurrentUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace GossipGastropodsBackEnd.Models
+{
+    public class CurrentUser
+    {
+        public Guid GUID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProfilePicture { get; set; }
+        public bool EmailVerified { get; set; }
+
+        public CurrentUser(IHttpContextAccessor httpContext)
+        {
+            GUID = Guid.Parse(httpContext?.HttpContext?.User?.FindFirst("guid")?.Value);
+            FirstName = httpContext?.HttpContext?.User?.FindFirst("firstName")?.Value;
+            LastName = httpContext?.HttpContext?.User?.FindFirst("lastName")?.Value;
+            ProfilePicture = httpContext?.HttpContext?.User?.FindFirst("profilePicture")?.Value;
+            EmailVerified = bool.Parse(httpContext?.HttpContext?.User?.FindFirst("VerifiedEmail")?.Value);
+        }
+    }
+}
