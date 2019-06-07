eb9e9f16c9fa6140d94a542f10d5afe6959c5bcb
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2019-06-07 02:09:59 +0100

    added basic user entity

---
+++ GossipGastropodsBackEnd/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GossipGastropodsBackEnd.Entities
+{
+    public class User
+    {
+        public Guid GUID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ProfilePicture { get; set; }
+        public bool EmailVerified { get; set; }
+    }
+}
