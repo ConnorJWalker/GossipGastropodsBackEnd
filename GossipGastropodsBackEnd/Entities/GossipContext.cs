e905479b44db9f8e7e0a174caf0be08136d58f66
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2019-06-07 02:06:36 +0100

    added database context

---
+++ GossipGastropodsBackEnd/Entities/GossipContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GossipGastropodsBackEnd.Entities
+{
+    public class GossipContext : DbContext
+    {
+        public GossipContext(DbContextOptions<GossipContext> options) : base(options) { }
+    }
+}
--- GossipGastropodsBackEnd/Startup.cs
+++ GossipGastropodsBackEnd/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GossipGastropodsBackEnd.Entities;
 using GossipGastropodsBackEnd.Helpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -28,6 +30,8 @@ namespace GossipGastropodsBackEnd
             EnvironmentSettings env = Configuration.GetSection("Environment").Get<EnvironmentSettings>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.Configure<EnvironmentSettings>(Configuration.GetSection("Environment"));
+            services.AddDbContext<GossipContext>(options => options.UseSqlServer(env.Database.ConnectionString));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
