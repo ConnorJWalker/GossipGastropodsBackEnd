34eba8e7fa08721a55b42a2a53cc00963cf28669
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2019-06-07 19:05:50 +0100

    added new comment end point

---
+++ GossipGastropodsBackEnd/Controllers/CommentsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using GossipGastropodsBackEnd.Models;
+using GossipGastropodsBackEnd.Helpers;
+using GossipGastropodsBackEnd.Entities;
+using GossipGastropodsBackEnd.Models.Http.Requests.Posts;
+
+namespace GossipGastropodsBackEnd.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json"), Consumes("application/json")]
+    public class CommentsController : ControllerBase
+    {
+        private readonly GossipContext context;
+        private readonly CurrentUser currentUser;
+        public CommentsController(GossipContext context, IHttpContextAccessor httpContext)
+        {
+            this.context = context;
+            currentUser = httpContext.GetCurrentUser();
+        }
+
+        [HttpPost("{postId}")]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult NewComment(int postId, [FromBody] NewCommentRequest request)
+        {
+            if (context.Posts.FirstOrDefault(p => p.Id == postId) == null)
+                return NotFound();
+
+            Comment comment = new Comment(request, currentUser, postId);
+            context.Comments.Add(comment);
+            context.SaveChanges();
+            return Ok(comment);
+        }
+    }
+}
---
+++ GossipGastropodsBackEnd/Models/Http/Requests/Posts/NewCommentRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GossipGastropodsBackEnd.Models.Http.Requests.Posts
+{
+    public class NewCommentRequest
+    {
+        [MinLength(3, ErrorMessage = "Comment body must be at least 3 charters long")]
+        public string Body { get; set; }
+    }
+}
