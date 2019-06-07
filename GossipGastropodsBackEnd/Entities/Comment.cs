e739b91efa17f33824d9fbeb10bdc16e60d33584
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2019-06-07 17:33:39 +0100

    added comments table

---
+++ GossipGastropodsBackEnd/Entities/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GossipGastropodsBackEnd.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string Body { get; set; }
+        public bool IsEdited { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public User Owner { get; set; }
+        public Post Post { get; set; }
+    }
+}
--- GossipGastropodsBackEnd/Migrations/20190607163030_AddCommentsTable.Designer.cs
+++ GossipGastropodsBackEnd/Migrations/20190607163030_AddCommentsTable.Designer.cs
@@ -4,14 +4,16 @@ using GossipGastropodsBackEnd.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GossipGastropodsBackEnd.Migrations
 {
     [DbContext(typeof(GossipContext))]
-    partial class GossipContextModelSnapshot : ModelSnapshot
+    [Migration("20190607163030_AddCommentsTable")]
+    partial class AddCommentsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
