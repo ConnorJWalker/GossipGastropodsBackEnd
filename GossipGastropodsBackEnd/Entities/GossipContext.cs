9775e2ee065faa68d55b072b2aefc1500aa3b536
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2019-06-07 13:54:49 +0100

    added constructor functions

--- GossipGastropodsBackEnd/Entities/GossipContext.cs
+++ GossipGastropodsBackEnd/Entities/GossipContext.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace GossipGastropodsBackEnd.Entities
 {
     public class GossipContext : DbContext
     {
         public GossipContext(DbContextOptions<GossipContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(u => u.GUID);
+        }
+
+        public DbSet<User> Users { get; set; }
     }
 }
--- GossipGastropodsBackEnd/Entities/User.cs
+++ GossipGastropodsBackEnd/Entities/User.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Security.Cryptography;
+using GossipGastropodsBackEnd.Models.Http.Requests.Auth;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
 namespace GossipGastropodsBackEnd.Entities
 {
@@ -11,5 +14,30 @@ namespace GossipGastropodsBackEnd.Entities
         public string Password { get; set; }
         public string ProfilePicture { get; set; }
         public bool EmailVerified { get; set; }
+
+        public User() { }
+
+        public User(SignupRequest request, string defaultProfilePicture)
+        {
+            FirstName = request.FirstName;
+            LastName = request.LastName;
+            Email = request.Email.ToLower();
+            Password = HashPassword(request.Password);
+            ProfilePicture = defaultProfilePicture;
+        }
+
+        private string HashPassword(string password)
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+                rng.GetBytes(salt);
+
+            return Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 1000,
+                numBytesRequested: 256 / 8));
+        }
     }
 }
