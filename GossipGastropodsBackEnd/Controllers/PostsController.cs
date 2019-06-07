21ff8c9a1d4f7c5cdaba4eaf8c1bd7dbddec31bf
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2019-06-07 15:38:33 +0100

    added new posts endpoint

---
+++ GossipGastropodsBackEnd/Controllers/PostsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GossipGastropodsBackEnd.Entities;
+using GossipGastropodsBackEnd.Helpers;
+using GossipGastropodsBackEnd.Models;
+using GossipGastropodsBackEnd.Models.Http.Requests.Posts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GossipGastropodsBackEnd.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/[controller]")]
+    [Produces("application/json"), Consumes("application/json")]
+    public class PostsController : ControllerBase
+    {
+        private readonly GossipContext context;
+        private readonly CurrentUser currentUser;
+        public PostsController(GossipContext context, IHttpContextAccessor httpContext)
+        {
+            this.context = context;
+            currentUser = httpContext.GetCurrentUser();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public IActionResult NewPost(NewPostRequest request)
+        {
+            Post post = new Post(request, currentUser);
+            context.Posts.Add(post);
+            context.SaveChanges();
+
+            return Created("", new { Post = post });
+        }
+    }
+}
---
+++ GossipGastropodsBackEnd/Models/Http/Requests/Posts/NewPostRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GossipGastropodsBackEnd.Models.Http.Requests.Posts
+{
+    public class NewPostRequest
+    {
+        [MinLength(5, ErrorMessage = "Post body must be at least 5 charters long")]
+        public string Body { get; set; }
+    }
+}
