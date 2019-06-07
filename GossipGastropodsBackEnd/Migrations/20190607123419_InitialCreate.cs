6cbd27f5189e94ce874dfb736f95d2d893108464
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2019-06-07 13:54:26 +0100

    added initial migration

---
+++ GossipGastropodsBackEnd/Migrations/20190607123419_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GossipGastropodsBackEnd.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    GUID = table.Column<Guid>(nullable: false, defaultValueSql: "NEWID()"),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    ProfilePicture = table.Column<string>(nullable: false),
+                    EmailVerified = table.Column<bool>(nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.GUID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
