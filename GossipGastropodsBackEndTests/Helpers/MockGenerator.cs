3913be902221e9394ecec2b67b568122e0610137
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2020-02-03 02:59:20 +0000

    added dbset generator for unit tests

---
+++ GossipGastropodsBackEndTests/Helpers/MockGenerator.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace GossipGastropodsBackEndTests.Helpers
+{
+    static class MockDbSetGenerator<T, L> where T : class where L : IQueryable
+    {
+        public static Mock<DbSet<T>> Gen(ref L list)
+        {
+            var mockSet = new Mock<DbSet<T>>();
+            mockSet.As<IQueryable<T>>()
+                .Setup(m => m.Provider)
+                .Returns(list.Provider);
+            mockSet.As<IQueryable<T>>()
+                .Setup(m => m.Expression)
+                .Returns(list.Expression);
+            mockSet.As<IQueryable<T>>()
+                .Setup(m => m.ElementType)
+                .Returns(list.ElementType);
+            mockSet.As<IQueryable<T>>()
+                .Setup(m => m.GetEnumerator())
+                .Returns(list.Cast<T>().GetEnumerator());
+
+            return mockSet;
+        }
+    }
+}
